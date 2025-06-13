817688e1107f315f1de9d3959437ed0309630ad0
cain. <caintheterrible@gmail.com>
2025-06-13 17:38:03 +0000

    UPDATED: included login route URL definition

--- backend/apps/authentication_module/urls.py
+++ backend/apps/authentication_module/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.authentication_module.views import register
+from apps.authentication_module.views import register, login
 
 urlpatterns=[
     path('register/', register),
+    path('login/', login),
 ]
